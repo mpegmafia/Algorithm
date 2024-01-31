84d0802ce9affca18ebf1b2a50434783da1f92ab
mpegmafia <156555695+mpegmafia@users.noreply.github.com>
2024-01-31 21:10:29 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 748 ms, Memory: 140376 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -6,41 +6,32 @@ import java.util.StringTokenizer;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		int N = Integer.parseInt(st.nextToken()); // 판 크기
-		int M = Integer.parseInt(st.nextToken()); // 합을 구하는 횟수
-
-		// 해당 정보를 2차원 배열에 누적합으로 담기
-		// arr[2][3]
-		// - 2행의 0열부터 3열까지의 누적합
-		int[][] arr = new int[N + 1][N + 1];
-		for (int i = 1; i <= N; i++) {
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[][] dp = new int[N+1][N+1];
+		for(int i=1; i<=N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 1; j <= N; j++) {
-				arr[i][j] = arr[i][j - 1] + Integer.parseInt(st.nextToken());
+			for(int j=1; j<=N; j++) {
+				dp[i][j] = Integer.parseInt(st.nextToken()) + dp[i-1][j] +dp[i][j-1] -dp[i-1][j-1];
 			}
 		}
-
-//		System.out.println(Arrays.deepToString(arr));
-		while (M-- > 0) {
+		
+		for(int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int x1 = Integer.parseInt(st.nextToken());
-			int y1 = Integer.parseInt(st.nextToken());
-			int x2 = Integer.parseInt(st.nextToken());
-			int y2 = Integer.parseInt(st.nextToken());
-
-			int sum = 0;
-			for (int x = x1; x <= x2; x++) {
-				sum += arr[x][y2];
-				sum -= arr[x][y1 - 1];
-			}
-			sb.append(sum).append('\n');
+			int y1= Integer.parseInt(st.nextToken());
+			int x1= Integer.parseInt(st.nextToken());
+			int y2= Integer.parseInt(st.nextToken());
+			int x2= Integer.parseInt(st.nextToken());
+			
+			int ans = dp[y2][x2] - dp[y1-1][x2] - dp[y2][x1-1] + dp[y1-1][x1-1];
+			sb.append(ans+"\n");
 		}
 		System.out.println(sb);
+
 	}
+
 }
