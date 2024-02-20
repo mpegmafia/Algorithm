c50df4886469b9e8ac0b21c165fd4792ca26fea1
mpegmafia <156555695+mpegmafia@users.noreply.github.com>
2024-02-20 17:49:23 +0900

    [Gold IV] Title: 연구소, Time: 472 ms, Memory: 87456 KB -BaekjoonHub

---
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -0,0 +1,113 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	/*
+	 * NxM의 직사각형인 연구소
+	 * 바이러스는 상하좌우로 움직이며 벽으로는 못움직임
+	 * 주어진 지도에 벽을 단 3개만 세워서 빈칸개수의 최댓값을 구하기
+	 * 벽의 설치좌표를 조합으로 구해서 모든 벽의 경우의수를 구한 다음 거기서 BFS를 돌려 0의 개수 찾기
+	 */
+
+	static int N;
+	static int M;
+	static int[][] graph;
+	static int ans;
+	static int[] picked;
+	static int[][] copygraph;
+	
+	static int[] dx = {1,0,-1,0};
+	static int[] dy = {0,1,0,-1};
+	static List<int[]> viruses = new ArrayList<>();
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		graph = new int[N][M];
+		copygraph = new int[N][M];
+		picked = new int[3];
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j=0; j<M; j++) {
+				graph[i][j] = Integer.parseInt(st.nextToken());
+				if (graph[i][j]==2) viruses.add(new int[] {i,j});
+			}
+		}
+		
+		combi(0,0);
+		System.out.println(ans);
+		
+
+	}
+	public static void combi(int depth, int start) {
+		if(depth==3) {
+			for(int i=0; i<N; i++) {
+				copygraph[i] = Arrays.copyOf(graph[i], graph[i].length);
+			}
+			
+			for(int i=0; i<3; i++) {
+				int r = picked[i]/M;
+				int c = picked[i]%M;
+				copygraph[r][c] = 1;
+			}
+			ans = Math.max(ans,BFS(copygraph));
+			return;
+		}
+		
+		for(int i= start; i<N*M; i++ ) {
+			int r = i/M;
+			int c = i%M;
+			if(graph[r][c] != 0) continue;
+			picked[depth] = i;
+			combi(depth+1, i+1);
+		}
+		
+		
+	}
+	
+	public static int BFS (int[][] map) {
+		Queue<int[]> q = new ArrayDeque<>();
+		for(int i=0; i<viruses.size(); i++) {
+			q.add(viruses.get(i));
+		}
+		int temp = 0;
+		
+		while(!q.isEmpty()) {
+			int y = q.peek()[0];
+			int x = q.peek()[1];
+			q.poll();
+			for(int i=0; i<4; i++) {
+				int ny = y +dy[i];
+				int nx = x +dx[i];
+				if(isInRange(ny, nx)&&map[ny][nx]==0) {
+					q.offer(new int[] {ny, nx});
+					map[ny][nx] = 2;
+				}
+				
+			}
+		}
+		
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<M; j++) {
+				if(map[i][j]==0) temp++;
+			}
+		}
+		
+		return temp;
+		
+	}
+	public static boolean isInRange(int y, int x) {
+		return y>=0&&y<N&&x>=0&&x<M;
+	}
+}
