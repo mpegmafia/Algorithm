e1e007a18d7bf9f9f75479a33a5c3a0ef878bbed
mpegmafia <156555695+mpegmafia@users.noreply.github.com>
2024-02-22 08:58:26 +0900

    [Gold IV] Title: 타일 채우기, Time: 80 ms, Memory: 11472 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+	import java.io.IOException;
+	import java.io.InputStreamReader;
+	
+	public class Main {
+	
+		public static void main(String[] args) throws IOException{
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			int N = Integer.parseInt(br.readLine());
+			int[] dp = new int[31];
+			dp[0] = 1;
+			dp[2] = 3;
+			
+			for(int i=4; i<=N; i++) {
+				dp[i] = dp[i-2]*3;
+				for(int j=i-4; j>=0; j-=2) {
+					dp[i] += dp[j]*2;
+				}
+			}
+			System.out.println(dp[N]);
+		}
+	
+	}
