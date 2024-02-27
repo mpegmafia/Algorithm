c492fdb08fb646597e5872547401998a371ad9ea
mpegmafia <156555695+mpegmafia@users.noreply.github.com>
2024-02-27 15:15:13 +0900

    [Gold IV] Title: 감시, Time: 248 ms, Memory: 23892 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -1,113 +1,177 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static long ans = Integer.MAX_VALUE;
-	static int[] dy = { 0, 1, 0, -1 };
-	static int[] dx = { 1, 0, -1, 0 };
-	static int[] nums = { 0, 4, 2, 4, 4, 1 };
+	/*
+	 * NxM크기의 직사각형
+	 * K개의 CCTV가 설치되어 있음 (최대 8개)
+	 * CCTV는 5가지
+	 * 1. 오른쪽
+	 * 2. 왼쪽, 오른쪽
+	 * 3. 위쪽, 오른쪽
+	 * 4. 왼쪽, 위쪽, 오른쪽
+	 * 5. 왼쪽, 위쪽, 오른쪽, 아래쪽
+	 * CCTV는 벽을 통과할 수 없음(같은 CCTV는 통과 가능)
+	 * CCTV는 회전할 수 있지만 항상 90도 방향으로만 회전함
+	 * CCTV의 방향들을 적절히 조절해서 사각지대의 최소크기를 구하라
+	 * 입력:
+	 * 	세로크기 N, 가로크기 M
+	 * 	N줄동안 그래프 상태, 1~5는 CCTV종류, 6은 벽, 0은 빈칸
+	 * 출력:
+	 * 	사각지대의 최소 크기
+	 * --------------
+	 * 사각지대를 돌리는 모든 경우의수를 구해서 각각 크기를 구한후 최솟값을 갱신해주면 된다.
+	 * 최악의경우 5^8 = 400만정도 시간은 될거같음
+	 * 그래프를 입력받을 때 CCTV만 따로 입력받아서 그것만 돌려보기
+	 */
+	
+	
+	
+	static class CCTV {
+		int y, x, no;
+		
+		int dir = -1;
 
-	public static void BackTracking(int[][] map, int y, int x) {
-		int[][] newmap = new int[map.length][];
-		for (int i = 0; i < map.length; i++) {
-			newmap[i] = Arrays.copyOf(map[i], map[i].length);
+		public CCTV(int y, int x, int no) {
+			super();
+			this.y = y;
+			this.x = x;
+			this.no = no;
 		}
-		for (int i = y; i < newmap.length; i++) {
-			for (int j = 0; j < newmap[y].length; j++) {
-				if (newmap[i][j] >= 1 && newmap[i][j] <= 5) {
-					for (int k = 0; k < nums[newmap[i][j]]; k++) {
-						BackTracking(FillMap(newmap, i, j, newmap[i][j], k), i, j);
-					}
-					return;
-				}
-			}
-		}
-		ans = Math.min(ans, 
-				Arrays.stream(newmap).flatMapToInt(Arrays::stream).filter(a -> a == 0).count()
-				);
-		return;
 	}
-
-	public static int[][] Forward(int[][] map, int y, int x, int num) {
-		int ny = y + dy[num];
-		int nx = x + dx[num];
-		while (ny >= 0 && ny < map.length && nx >= 0 && nx < map[ny].length) {
-			if (map[ny][nx] >= 1 && map[ny][nx] <= 5) {
-				ny += dy[num];
-				nx += dx[num];
-				continue;
-			} else if (map[ny][nx] == 0 || map[ny][nx] == 7) {
-				map[ny][nx] = 7;
-				ny += dy[num];
-				nx += dx[num];
-				continue;
-			} else if (map[ny][nx] == 6)
-				break;
-		}
-		return map;
-	}
-
-	// dir = 어떤cctv인지 num = 지금은 어떤방향인지
-	public static int[][] FillMap(int[][] map, int y, int x, int dir, int num) {
-		int[][] newmap = new int[map.length][];
-		for (int i = 0; i < map.length; i++) {
-			newmap[i] = Arrays.copyOf(map[i], map[i].length);
-		}
-		newmap[y][x] = 7;
-
-		if (dir == 1) {
-			return Forward(newmap, y, x, num);
-		}
-
-		if (dir == 2) {
-			newmap = Forward(newmap, y, x, num);
-			newmap = Forward(newmap, y, x, num + 2);
-			return newmap;
-		}
-		if (dir == 3) {
-			newmap = Forward(newmap, y, x, num);
-			newmap = Forward(newmap, y, x, (num + 1) % 4);
-			return newmap;
-		}
-		if (dir == 4) {
-			newmap = Forward(newmap, y, x, num);
-			newmap = Forward(newmap, y, x, (num + 1) % 4);
-			newmap = Forward(newmap, y, x, (num + 2) % 4);
-			return newmap;
-		}
-		if (dir == 5) {
-			newmap = Forward(newmap, y, x, num);
-			newmap = Forward(newmap, y, x, num + 1);
-			newmap = Forward(newmap, y, x, num + 2);
-			newmap = Forward(newmap, y, x, num + 3);
-			return newmap;
-		}
-		return newmap;
-	}
+	
+	static int[] dy = {0,1,0,-1}; //우, 하, 좌, 상
+	static int[] dx = {1,0,-1,0};
+	static int[] cnt = {0, 4, 2, 4, 4, 1}; //CCTV별 회전 횟수
+	
+	static int N;
+	static int M;
+	static int[][] graph;
+	static int[][] copygraph;
+	
+	static List<CCTV> list = new ArrayList<>();
+	
+	static int ans = Integer.MAX_VALUE;
+	
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
+		
 		st = new StringTokenizer(br.readLine());
-
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-
-		int[][] graph = new int[N][M];
-		for (int i = 0; i < N; i++) {
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		graph = new int[N][M];
+		
+		copygraph = new int[N][M];
+		
+		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < M; j++) {
+			for(int j=0; j<M; j++) {
 				graph[i][j] = Integer.parseInt(st.nextToken());
+				if(graph[i][j]>=1&&graph[i][j]<=5) list.add(new CCTV(i, j, graph[i][j]));
 			}
 		}
-
-		BackTracking(graph, 0, 0);
+		DFS(0);
 		System.out.println(ans);
 
+	}
+	
+	public static int chk() {
+		int temp = 0;
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<M; j++) {
+				if(copygraph[i][j]==0) temp++;
+			}
+		}
+		return temp;
+	}
+	
+	public static void DFS(int depth) {
+		if (depth==list.size()) { // 모든 CCTV의 방향을 다 골랐을때
+			for(int i=0; i<N; i++) {
+				System.arraycopy(graph[i], 0, copygraph[i], 0, M);
+			}
+			
+			fillGraph(copygraph);
+
+			ans = Math.min(ans, chk());
+			return;
+		}
+		
+		CCTV c = list.get(depth);
+		for(int i=0; i<cnt[c.no]; i++) {
+			c.dir = i;
+			DFS(depth+1);
+		}
+		
+	}
+	
+	
+	public static void fillGraph(int[][] map) {
+		
+		for(CCTV c : list) {
+			int y = c.y;
+			int x = c.x;
+			if(c.no==1) {
+				forward(y, x, c.dir);
+				
+			}
+			else if(c.no==2) {
+				forward(y, x, c.dir);
+				forward(y, x, c.dir+2);
+				
+			}
+			else if(c.no==3) {
+				forward(y, x, c.dir);
+				forward(y, x, (c.dir+1)%4);
+				
+			}
+			else if(c.no==4) {
+				forward(y, x, c.dir);
+				forward(y, x, (c.dir+1)%4);
+				forward(y, x, (c.dir+2)%4);
+				
+			}
+			else if(c.no==5) {
+				forward(y, x, c.dir);
+				forward(y, x, c.dir+1);
+				forward(y, x, c.dir+2);
+				forward(y, x, c.dir+3);
+				
+			}
+			
+		}
+		
+		
+	}
+	
+	public static void forward(int y, int x, int dir) {
+		int ny = y;
+		int nx = x;
+		while(true) {
+			if(!isInRange(ny,nx)) break;
+			if(copygraph[ny][nx] == 6) break;
+			if(copygraph[ny][nx]>=1&&copygraph[ny][nx]<=7) {
+				ny = ny+ dy[dir];
+				nx = nx+ dx[dir];
+				continue;
+			} else {
+				copygraph[ny][nx] = 7;
+				ny = ny+dy[dir];
+				nx = nx+dx[dir];
+			}
+		}
+		
+	}
+	public static boolean isInRange(int y, int x) {
+		return y>=0&y<N&&x>=0&&x<M;
 	}
 
 }
