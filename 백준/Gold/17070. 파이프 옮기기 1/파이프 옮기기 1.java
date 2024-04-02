f6613e77ae457d49eeb4fede716d924fc752cb89
mpegmafia <156555695+mpegmafia@users.noreply.github.com>
2024-04-02 13:00:50 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 80 ms, Memory: 11592 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,35 +1,28 @@
-/*
- * 오른쪽
- * 오른쪽아래
- * 아래
- * 회전은 45도만 시킬 수 있다.
- * 오른쪽일시 오른쪽, 오른쪽아래만
- * 오른쪽아래일시 오른쪽, 아래, 오른쪽아래 셋다 가능
- * 아래일시 아래, 오른쪽아래만
- * 1,2 에서 N,N까지 가는 방법의 개수 구하기
- * 
- */
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N;
-	static int[][] graph;
-	static int ans;
-	
-	static int[][] delta = { {0,1},
-			{1,1},
-			{1,0}		
-	};
-	public static void main(String[] args)  throws IOException{
+	/*
+	 * dp[3][N+1][N+1] 로 선언
+	 * 0은 가로
+	 * 1은 대각
+	 * 2는 세로 방향
+	 * 가로방향은 가로방향의 x-1이나 대각선방향의 x-1을 더하면됨
+	 * 세로방향은 세로방향의 y-1이나 대각선방향의 y-1을 더하면됨
+	 * 대각선 방향은 가로방향의 y-1,x-1이나 세로방향의 x-1, y-1, 대각선방향의 y-1, x-1을 더하면됨
+	 * 
+	 * 
+	 */
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
-		N = Integer.parseInt(br.readLine());
-		graph = new int[N][N];
+		int N = Integer.parseInt(br.readLine());
+		int[][] graph = new int[N][N];
+		int[][][] dp = new int[3][N][N];
 		
 		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -37,66 +30,31 @@ public class Main {
 				graph[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
+		for(int i=1; i<N; i++) {
+			if(graph[0][i]==1) break;
+			dp[0][0][i] = 1;
+			
+		}
 		
-		DFS(0,1,0);
+		
+		for(int i=1; i<N; i++) {
+			for(int j=1; j<N; j++) {
+				if(graph[i][j]==0) {
+					dp[0][i][j] =  dp[0][i][j-1] + dp[1][i][j-1];
+					dp[2][i][j] =  dp[2][i-1][j]+ dp[1][i-1][j];
+				}
+				if(graph[i][j]==0&&graph[i-1][j]==0&&graph[i][j-1]==0) {
+					dp[1][i][j] =  dp[0][i-1][j-1] + dp[1][i-1][j-1] + dp[2][i-1][j-1];
+				}
+			}
+		}
+		
+		
+		
+		
+		
+		int ans = dp[0][N-1][N-1]+dp[1][N-1][N-1]+dp[2][N-1][N-1];
 		System.out.println(ans);
-		
-	}
-	private static void DFS(int y, int x, int dir) {
-		if(y==N-1&&x==N-1) {
-			ans++;
-			return;
-		}
-		
-		if(dir==0) {
-			for(int i=0; i<2; i++) {
-				int ny = y+delta[i][0];
-				int nx = x+delta[i][1];
-				if(!isInRange(ny, nx)) continue;
-				if(chk(ny,nx,i)) continue;
-				DFS(ny, nx, i);				
-			}
-			
-		} else if(dir==1) {
-			for(int i=0; i<3; i++) {
-				int ny = y+delta[i][0];
-				int nx = x+delta[i][1];
-				if(!isInRange(ny, nx)) continue;
-				if(chk(ny,nx,i)) continue;
-				DFS(ny, nx, i);				
-			}
-			
-		} else {
-			for(int i=1; i<3; i++) {
-				int ny = y+delta[i][0];
-				int nx = x+delta[i][1];
-				if(!isInRange(ny, nx)) continue;
-				if(chk(ny,nx,i)) continue;
-				DFS(ny, nx, i);				
-			}
-			
-		}
-		
-		
-		
-		
-	}
-	
-	public static boolean isInRange(int y, int x) {
-		return y>=0&&y<N&&x>=0&&x<N;
-	}
-	
-	public static boolean chk(int y, int x, int dir) {
-		if(dir==1) {
-			if(graph[y][x]==0&&graph[y-1][x]==0&&graph[y][x-1]==0) {
-				return false;
-			} else return true;
-			
-		} else {
-			if(graph[y][x]==0) return false;
-			else return true;
-		}
-		
 	}
 
 }
