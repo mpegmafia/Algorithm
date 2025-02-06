9e79537785d3eefba0c866062ae13a54fd497cb7
이찬호 <156555695+mpegmafia@users.noreply.github.com>
2025-02-06 18:02:55 +0900

    [Gold V] Title: 최소비용 구하기, Time: 484 ms, Memory: 57732 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+/*
+A에서 B까지 가는데 걸리는 최소 비용
+=====================
+입력:
+    N (1<=N<=1,000)
+    M (1<=M<=100,000)
+    M줄동안 간선 정보(단방향, 출발, 도착, 비용)
+    출발점, 도착점  (무조건 갈 수 있음)
+=====================
+다익스트라 알고리즘인가?
+출발점에서 시작해 도착지점이 나올때까지 최소가중치로 그래프 순회하기
+출발 node의 간선정보를 pq에 집어넣자
+pq에서 제일 weight가 낮은 node 부터 poll
+현재 node 방문처리
+현재 node가 도착 노드인지 확인
+    도착node라면 현재까지의 weight return
+그 후 해당 node의 간선들을 전부 순회
+만약 해당 간선 도착노드가 방문안한 노드라면
+    현재까지의 weight를 더해서 pq에 넣기
+ */
+public class Main {
+    static int N,M;
+    static List<List<int[]>> adjList = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+        for(int i=0; i<=N; i++){
+            adjList.add(new ArrayList<>());
+        }
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            adjList.get(s).add(new int[]{e,w});
+        }
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        int ans = Dijkstra(start, end);
+        System.out.println(ans);
+
+
+
+    }
+
+    private static int Dijkstra(int start, int end) {
+        boolean[] visited = new boolean[N+1];
+        visited[start] = true;
+        PriorityQueue<int[]> pq = new PriorityQueue<>( (a,b) -> a[1] - b[1]);
+        pq.add(new int[]{start,0});
+        int tmp = 0;
+        while(!pq.isEmpty()){
+            int[] cur = pq.poll();
+            if(cur[0] == end){
+                tmp = cur[1];
+                break;
+            }
+            visited[cur[0]] = true;
+            for(int[] adj : adjList.get(cur[0])){
+                if(visited[adj[0]]) continue;
+                pq.add(new int[]{adj[0], cur[1]+adj[1]});
+            }
+        }
+        return tmp;
+    }
+}
