27a87611577a0f438b85e734aa48f1d42cf31237
이찬호 <156555695+mpegmafia@users.noreply.github.com>
2025-02-10 18:41:54 +0900

    [Gold III] Title: 공주님의 정원, Time: 428 ms, Memory: 50032 KB -BaekjoonHub

--- 백준/Gold/2457. 공주님의 정원/공주님의 정원.java
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.java
@@ -1,76 +1,78 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 	
-	
-	
-	public static void main(String[] args) throws IOException{
+	static class Flower implements Comparable<Flower>{
+		int std, endd;
+
+		public Flower(int std, int endd) {
+			super();
+			this.std = std;
+			this.endd = endd;
+		}
+
+		@Override
+		public int compareTo(Flower o) {
+			// TODO Auto-generated method stub
+			return Integer.compare(this.std, o.std);
+		}
+	}
+	public static void main(String[] args) throws IOException {
+		
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
+		
 		int N = Integer.parseInt(br.readLine());
+		Flower[] flowers = new Flower[N];
 		
-		
-		PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->(a[0]==b[0]?a[1]-b[1]:a[0]-b[0]));
-		//시작월, 시작일, 종료월, 종료일 순, 종료월 기준으로 정렬
 		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken())*100;
 			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken())*100;
 			int d = Integer.parseInt(st.nextToken());
-			pq.offer(new int[] {a,b,c,d});
+			flowers[i] = new Flower(a+b, c+d);
 		}
 		
+		Arrays.sort(flowers);
 		int ans = 0;
-		int startm = 1;
-		int startd = 1;
-		int endm = 3;
-		int endd = 1;
 		int cnt = 0;
-		int curmon = 0;
-		int curday = 0;
-		
-		
-		while(!pq.isEmpty()) {
-			int[] d = pq.poll();
-			int flag = 0;
+		int endday = 301;
+		int idx = 0;
+		int curend = 0;
+		while(curend<1201) {
 			
+			boolean find = false;
 			
-			while(d!=null && (d[0]<endm||d[0]==endm&&d[1]<=endd)) { //만약 현재 꽃이 종료일 이전에  심을 수 있고
-				if (flag==1) d = pq.poll();
-				if(d[2]>curmon||(d[2]==curmon&&d[3]>curday)) {
-					curmon = d[2];
-					curday = d[3];
+			for(int i=idx; i<flowers.length; i++) {
+				
+				if(flowers[i].std<=endday) {
+					if(curend<flowers[i].endd) {
+						curend = flowers[i].endd;
+						find = true;
+					}
+				} else {
+					idx = i;
+					break;
 				}
-				flag = 1;
-				d = pq.peek();
 				
 			}
-			cnt++;
-//			System.out.println(Arrays.toString(d));
-//			System.out.println("curmon : " + curmon + " curday : "+curday+ "cnt : "+ cnt);
-			if(curmon>=12) {
-				ans = cnt;
-				break;
-			}
-			endm = curmon;
-			endd = curday;
+			if(find) {
+				cnt++;
+				endday = curend;
+			} else break;
 			
 		}
-		
-		
+		if(curend>=1201) ans = cnt;
 		System.out.println(ans);
 		
+		
+
 	}
-	
 
 }
