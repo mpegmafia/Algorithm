78c3c566da70a663106ba958924938e678e90fc6
이찬호 <156555695+mpegmafia@users.noreply.github.com>
2025-02-18 23:44:32 +0900

    [Bronze V] Title: A+B - 3, Time: 64 ms, Memory: 11604 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<T; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a+b).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
